d762962fdb4e33f853692a7cce9a37c5cf7573a1
Leon Brochmann <Leon.Brochmann@ottogroup.com>
2024-11-23 23:00:56 +0100

    added analysis code,graphics and präsentation

---
+++ Readme.md
@@ -0,0 +1,56 @@
+﻿# Data Analysis Project
+
+This repository contains code and data for analyzing bike trip and collision data. The project includes functions for loading, cleaning, and visualizing the data.  
+Main Recipient is the AXA Innovation Lab
+
+## Table of Contents
+
+- [Installation](#installation)
+- [General Information](#general-information)
+- [Functions](#functions)
+- [Usage](#usage)
+- [License](#license)
+
+## Installation
+
+1. Clone the repository:
+    ```sh
+    git clone https://github.com/leonbrochmann/CitiBike.git
+    ```
+
+2. Navigate to the project directory:
+    ```sh
+    cd your folder name
+    ```
+
+3. Create a virtual environment and activate it:
+    Unfortunatly i had no time to set up poetry, so no lock file is included
+
+4. Install the required packages(As soon as the poetry toml is added):
+    ```sh
+    poetry install
+    ```
+## General Information before anything is executed  
+1. All of this code was done locally on a pc with limited resources.  
+Therefore all parts of the analysis where split into each year and sometimes even into months of the year.  
+If possible, with available ressources, build a datamart for the whole database and another for all nypd data or comparable company data to make data access more efficient.  
+2. For data before 2020, use the 2015.ipynb file, otherwise the 2020-2024.ipynb .
+## Usage
+
+1. Load and clean data:
+   There are 2 distinct methods to load the data currently:  
+   For data before 2020 use the method **load_and_rename_csv_files**  
+   For data starting 2020 use the method **load_and_rename_csv_files_2020**  
+   Both are found in the helper.py, and need all the column names they use so that you avoid data issued, especially the datetimes.
+
+2. Timedata format:  
+Almost all of the datafiles have timedata issues, so after converting them into a pandas datetime you can used the function **seconds_to_hhmmss** to convert seconds to hhmmss for better readability.
+
+
+## Next Steps  
+1. Demand Forecast at Stations:  
+    One possible next step would be to create a forecast service to see how many people need bikes at certain stations and current traffic in NYC to predict whether or not a insurance would be important.  
+2. Compare the data with simliar scooter services to see their rates of accidents, demand and even times of lending.  
+
+## License
+The data used in this repo completely belongs to citibike. They hold all rights to the data.
--- helper.py
+++ helper.py
@@ -2,26 +2,278 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from pathlib import Path
+import os
 
-def load_folder(name):
 
-    path = f'{name}/'  # or unix / linux / mac path
+def load_and_clean_csv(file_path):
+    """
+    Load a single CSV file and clean its data.
 
-    # Get the files from the path provided in the OP
-    files = list(Path(path).glob('*.csv')) # .rglob to get subdirectories
+    Parameters:
+    file_path (str): The path to the CSV file.
 
-    # Read the files into dataframes
-    dfs = [pd.read_csv(f) for f in files]
+    Returns:
+    pd.DataFrame: A cleaned DataFrame.
+    """
+    df = pd.read_csv(file_path)
+    
+    first_column_name = df.columns[0]
+    
+    # Rename all columns to the first column name
 
-    # Combine the list of dataframes
-    df = pd.concat(dfs, ignore_index=True)
+    # Rename columns if necessary
+    column_mapping = {
+        'start_time': 'starttime',
+        'stop_time': 'stoptime',
+        'trip_duration': 'tripduration',
+        'start_station_id': 'start_station_id',
+        'end_station_id': 'end_station_id',
+        'bike_id': 'bikeid',
+        'user_type': 'usertype',
+        'birth_year': 'birth_year',
+        'gender': 'gender'
+    }
+    df.rename(columns=column_mapping, inplace=True)
 
-    # Add a new column
-    df['Source'] = np.repeat([f'S{i}' for i in range(len(dfs))], [len(df) for df in dfs])
+    # Convert columns to appropriate types
+    if 'starttime' in df.columns:
+        df['starttime'] = pd.to_datetime(df['starttime'], errors='coerce')
+    if 'stoptime' in df.columns:
+        df['stoptime'] = pd.to_datetime(df['stoptime'], errors='coerce')
+    if 'tripduration' in df.columns:
+        df['tripduration'] = pd.to_numeric(df['tripduration'], errors='coerce')
+    if 'start_station_id' in df.columns:
+        df['start_station_id'] = pd.to_numeric(df['start_station_id'], errors='coerce')
+    if 'end_station_id' in df.columns:
+        df['end_station_id'] = pd.to_numeric(df['end_station_id'], errors='coerce')
+    if 'bikeid' in df.columns:
+        df['bikeid'] = pd.to_numeric(df['bikeid'], errors='coerce')
+    if 'usertype' in df.columns:
+        df['usertype'] = df['usertype'].astype('category')
+    if 'birth_year' in df.columns:
+        df['birth_year'] = pd.to_numeric(df['birth_year'], errors='coerce')
+    if 'gender' in df.columns:
+        df['gender'] = pd.to_numeric(df['gender'], errors='coerce')
+    
     return df
+
+def load_and_clean_2016_csv(file_path):
+    """
+    Load a single CSV file from 2016 and clean its data.
+
+    Parameters:
+    file_path (str): The path to the CSV file.
+
+    Returns:
+    pd.DataFrame: A cleaned DataFrame.
+    """
+    df = pd.read_csv(file_path)
+    
+    # Ensure all column names are in lowercase
+    df.columns = df.columns.str.strip().str.lower().str.replace(' ', '_').str.replace(r'[^\w\s]', '', regex=True)
+    
+    column_mapping = {
+        'Trip Duration': 'tripduration',
+        'Start Time': 'starttime',
+        'Stop Time': 'stoptime',
+        'Bike ID': 'bikeid',
+        'User Type': 'usertype'
+    }
+    df.rename(columns=column_mapping, inplace=True)
+
+    # Convert columns to appropriate types
+    if 'starttime' in df.columns:
+        df['starttime'] = pd.to_datetime(df['starttime'], errors='coerce')
+    if 'stoptime' in df.columns:
+        df['stoptime'] = pd.to_datetime(df['stoptime'], errors='coerce')
+    if 'tripduration' in df.columns:
+        df['tripduration'] = pd.to_numeric(df['tripduration'], errors='coerce')
+    if 'start_station_id' in df.columns:
+        df['start_station_id'] = pd.to_numeric(df['start_station_id'], errors='coerce')
+    if 'end_station_id' in df.columns:
+        df['end_station_id'] = pd.to_numeric(df['end_station_id'], errors='coerce')
+    if 'bikeid' in df.columns:
+        df['bikeid'] = pd.to_numeric(df['bikeid'], errors='coerce')
+    if 'usertype' in df.columns:
+        df['usertype'] = df['usertype'].astype('category')
+    if 'birth_year' in df.columns:
+        df['birth_year'] = pd.to_numeric(df['birth_year'], errors='coerce')
+    if 'gender' in df.columns:
+        df['gender'] = pd.to_numeric(df['gender'], errors='coerce')
+    
+    return df
+
+def load_folder(folder_path, year = None):
+    """
+    Load all CSV files in the specified folder into a single pandas DataFrame.
+
+    Parameters:
+    folder_path (str): The path to the folder containing the CSV files.
+
+    Returns:
+    pd.DataFrame: A DataFrame containing the concatenated data from all CSV files.
+    """
+    # Get the list of all CSV files in the folder
+    files = list(Path(folder_path).glob('*.csv'))
+
+    # Read each CSV file into a DataFrame and store them in a list
+    # Read and clean each CSV file into a DataFrame and store them in a list
+    if year == 2016:
+        dfs = [load_and_clean_2016_csv(file) for file in files]
+    else:
+        dfs = [load_and_clean_csv(file) for file in files]
+
+    # Concatenate all DataFrames in the list into a single DataFrame
+    combined_df = pd.concat(dfs, ignore_index=True)
+
+    return combined_df
 
 def seconds_to_hhmmss(seconds):
     hours = seconds // 3600
     minutes = (seconds % 3600) // 60
     seconds = seconds % 60
     return f"{int(hours):02}:{int(minutes):02}:{int(seconds):02}"
+
+
+
+def load_and_rename_csv_files(folder_path, column_names):
+    """
+    Load all CSV files in the specified folder into a single pandas DataFrame,
+    rename columns for each DataFrame to the specified list of column names,
+    and concatenate them into a single DataFrame.
+
+    Parameters:
+    folder_path (str): The path to the folder containing the CSV files.
+    column_names (list): The list of column names to rename each DataFrame.
+
+    Returns:
+    pd.DataFrame: A DataFrame containing the concatenated data from all CSV files with renamed columns.
+    """
+    # Initialize a list to store DataFrames
+    dfs = []
+
+    # Get the list of all CSV files in the folder
+    files = list(Path(folder_path).glob('*.csv'))
+
+    # Read and clean each CSV file into a DataFrame and append to the list
+    for file in files:
+        df = pd.read_csv(file)
+        
+        # Rename columns to the specified list of column names
+        df.columns = column_names
+
+        df['starttime'] = pd.to_datetime(df['starttime'], errors='coerce')
+        df['stoptime'] = pd.to_datetime(df['stoptime'], errors='coerce')
+
+        # Append the cleaned DataFrame to the list
+        dfs.append(df)
+
+    # Concatenate all DataFrames in the list into a single DataFrame
+    combined_df = pd.concat(dfs, ignore_index=True)
+
+    return combined_df
+
+
+
+def load_and_rename_csv_files_2020(folder_path, column_names):
+    """
+    Load all CSV files in the specified folder into a single pandas DataFrame,
+    rename columns for each DataFrame to the specified list of column names,
+    and concatenate them into a single DataFrame.
+
+    Parameters:
+    folder_path (str): The path to the folder containing the CSV files.
+    column_names (list): The list of column names to rename each DataFrame.
+
+    Returns:
+    pd.DataFrame: A DataFrame containing the concatenated data from all CSV files with renamed columns.
+    """
+    # Initialize a list to store DataFrames
+    dfs = []
+
+    # Get the list of all CSV files in the folder
+    files = list(Path(folder_path).glob('*.csv'))
+
+    # Read and clean each CSV file into a DataFrame and append to the list
+    for file in files:
+        df = pd.read_csv(file)
+        
+        # Rename columns to the specified list of column names
+        df.columns = column_names
+
+        df['started_at'] = pd.to_datetime(df['started_at'], errors='coerce')
+        df['ended_at'] = pd.to_datetime(df['ended_at'], errors='coerce')
+
+        # Append the cleaned DataFrame to the list
+        dfs.append(df)
+
+    # Concatenate all DataFrames in the list into a single DataFrame
+    combined_df = pd.concat(dfs, ignore_index=True)
+
+    return combined_df
+
+import pandas as pd
+from pathlib import Path
+
+def load_and_rename_2024_07(folder_path, column_names):
+    """
+    Load all CSV files in the specified folder into a single pandas DataFrame,
+    drop the first column for each DataFrame, rename columns to the specified list of column names,
+    and concatenate them into a single DataFrame.
+
+    Parameters:
+    folder_path (str): The path to the folder containing the CSV files.
+    column_names (list): The list of column names to rename each DataFrame.
+
+    Returns:
+    pd.DataFrame: A DataFrame containing the concatenated data from all CSV files with renamed columns.
+    """
+    # Initialize a list to store DataFrames
+    dfs = []
+
+    # Get the list of all CSV files in the folder
+    files = list(Path(folder_path).glob('*.csv'))
+
+    # Read and clean each CSV file into a DataFrame and append to the list
+    for file in files:
+        df = pd.read_csv(file)
+        
+        # Drop the first column
+        df.drop(df.columns[0], axis=1, inplace=True)
+        
+        # Rename columns to the specified list of column names
+        df.columns = column_names
+
+        # Convert started_at and ended_at columns to datetime
+        df['started_at'] = pd.to_datetime(df['started_at'], errors='coerce')
+        df['ended_at'] = pd.to_datetime(df['ended_at'], errors='coerce')
+
+        # Append the cleaned DataFrame to the list
+        dfs.append(df)
+
+    # Concatenate all DataFrames in the list into a single DataFrame
+    combined_df = pd.concat(dfs, ignore_index=True)
+
+    return combined_df
+
+
+def create_folder(path, folder_name):
+    """
+    Create a folder in the specified path with the given name.
+
+    Parameters:
+    path (str): The path where the folder should be created.
+    folder_name (str): The name of the folder to be created.
+
+    Returns:
+    str: The full path of the created folder.
+    """
+    # Combine the path and folder name to get the full path
+    full_path = os.path.join(path, folder_name)
+
+    # Create the folder if it does not exist
+    os.makedirs(full_path, exist_ok=True)
+
+    return full_path
+
+# Example usage
